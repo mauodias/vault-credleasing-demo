2dbaa35885aa04b469aa00d3313e44e7f662e556
Maurício Dias <mau.dias@gmail.com>
2019-02-18 20:44:53 -0300

    Permite qualquer role usar o endpoint de postgres no vault

--- _scripts/configdb.sh
+++ _scripts/configdb.sh
@@ -4,7 +4,7 @@ vault secrets enable database
 
 vault write -address=$VAULT_ADDR database/config/postgres \
     plugin_name=postgresql-database-plugin \
-    allowed_roles="read_only" \
+    allowed_roles="*" \
     connection_url="postgresql://{{username}}:{{password}}@postgres:5432?sslmode=disable" \
     username="postgres" \
     password="securepassword"
