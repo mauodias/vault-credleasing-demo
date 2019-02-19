f4276d69ff7a2fbf9f1e70e48d9219df103ed876
hscorrea <habyte@gmail.com>
2019-02-18 21:13:32 -0300

    Axios to make requests to vault

--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 
 app.use(express.static('public'))
@@ -8,7 +9,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/vault_creds', (req, res) => {
-  res.sendStatus(200)
+  axios.post('http://vault:8200/v1/auth/userpass/login/app', {
+      password: 'app'
+    })
+    .then(response => {
+      console.log(response.body)
+      res.sendStatus(response.statusCode)
+    })
+    .catch(error => {
+      console.log(error)
+      res.sendStatus(500)
+    })
 })
 
 app.get('/db_read', (req, res) => {
